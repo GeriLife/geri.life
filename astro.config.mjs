c47df5301184dac16a912a8f390220c9daefe1a8
Brylie Christopher Oxley <brylie@protonmail.com>
2025-04-06 10:03:46 +0300

    Refactor links in Layout.astro and index.astro to use BASE_URL for improved routing consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://gerilife.github.io',
+  base: 'geri.life',
   integrations: [
     sitemap(),
     icon(),
   ],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [ tailwindcss() ]
   }
 });
